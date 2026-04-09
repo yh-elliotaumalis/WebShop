9a5d8a926e4ef689efe44429f68e7509abd4fbaf
Peter Gergawy <pgergawy@gmail.com>
2026-04-09 20:20:42 +0300

    Ändra från bool till string

---
+++ WebShop/WebShop/Validator/SearchValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace WebShop.Presentation.Validator;
+
+public class SearchValidator
+{
+    public string? Validate(string input)
+    {
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            return null;
+        }
+        return "Sökning får inte vara tom";
+    }
+}

a49340a78e289f120cbf16b7241876d8a4a09a96
Peter Gergawy <pgergawy@gmail.com>
2026-04-09 10:52:13 +0300

    Added namespace

--- WebShop/Webshop.Application/Interfaces/IOrderService.cs
+++ WebShop/Webshop.Application/Interfaces/IOrderService.cs
@@ -1,9 +1,13 @@
 ﻿using Webshop.Domain.Entitites;
 using Webshop.Domain.Enums;
 
-public interface IOrderService
+namespace Webshop.Application.Interfaces
 {
-    Task<Order?> GetByIdAsync(Guid id);
-    Task<IEnumerable<Order>> GetByCustomerAsync(Guid kundId);
-    Task<Order> CreateOrderAsync(Guid kundId, List<ProduktOrder> produkter, Guid fraktOmbudId, Betalsätt betalsätt);
+    public interface IOrderService
+    {
+        Task<Order?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Order>> GetByCustomerAsync(Guid kundId);
+        Task<Order> CreateOrderAsync(Guid kundId, List<ProduktOrder> produkter, Guid fraktOmbudId, Betalsätt betalsätt);
+    }
+
 }

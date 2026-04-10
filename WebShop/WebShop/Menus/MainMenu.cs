edd5642ca9ac86616d8ead58dbdd77ebe4239a47
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-04-10 14:18:39 +0200

    Jag har fixat det och använder basklassens ShowMenu nu

--- WebShop/WebShop/Menus/MainMenu.cs
+++ WebShop/WebShop/Menus/MainMenu.cs
@@ -35,54 +35,8 @@ public class MainMenu : MenuBase
 
         return false;
     }
-    private void WriteCentered(string text)
-    {
-        int windowWidth = Console.WindowWidth;
-        int textLength = text.Length;
-        int spaces = (windowWidth - textLength) / 2;
-        Console.WriteLine(new string(' ', spaces) + text);
-    }
-    private void ShowWelcome()
-    {
-        WriteCentered("Välkommen till KläderShoppen!");
-        WriteCentered("************************");
-        Console.WriteLine();
 
-       
-        WriteCentered("Här Kan Du Bläddra Bland Produkter Och Hantera Ditt Konto.");
-        WriteCentered("Använd Piltangenterna För Att Navigera Och Enter För Att Välja.");
-        Console.WriteLine();
-    }
-    //Show welcome Text
-    public new void ShowMenu(string title)
-    {
-        int selectedIndex = 0;
 
-        while (true)
-        {
-            Console.Clear();
 
-            ShowWelcome();
-
-            PrintMenu(selectedIndex, title);
-
-            var key = Console.ReadKey(true).Key;
-
-            switch (key)
-            {
-                case ConsoleKey.UpArrow:
-                    selectedIndex = (selectedIndex - 1 + _options.Length) % _options.Length;
-                    break;
-
-                case ConsoleKey.DownArrow:
-                    selectedIndex = (selectedIndex + 1) % _options.Length;
-                    break;
-
-                case ConsoleKey.Enter:
-                    if (ExecuteChoice(selectedIndex))
-                        return;
-                    break;
-            }
-        }
-    }
 }
+

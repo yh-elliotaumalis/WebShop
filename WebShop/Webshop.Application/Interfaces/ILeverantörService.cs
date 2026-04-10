56502131c9cdf23feb6ceac9bc18c5096ea4a0a3
Elliot <233755458+yh-elliotaumalis@users.noreply.github.com>
2026-04-10 10:29:11 +0200

    Add ILeverantörRepository / Service

---
+++ WebShop/Webshop.Application/Interfaces/ILeverantörService.cs
@@ -0,0 +1,12 @@
+﻿using Webshop.Domain.Entitites;
+
+namespace Webshop.Application.Interfaces;
+
+public interface ILeverantörService
+{
+    Task<IEnumerable<Leverantör>> GetAllAsync();
+    Task<Leverantör?> GetByIdAsync(Guid id);
+    Task AddAsync(Leverantör leverantör);
+    Task UpdateAsync(Leverantör leverantör);
+    Task DeleteAsync(Guid id);
+}

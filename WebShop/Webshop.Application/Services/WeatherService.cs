a2677a5c8fba6155e524cfc2c2299daee14e747b
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-04-12 23:00:30 +0200

    Add Weather API service (not yet integrated into UI)

---
+++ WebShop/Webshop.Application/Services/WeatherService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Webshop.Application.Models;
+
+namespace Webshop.Application.Services
+{
+    internal class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+        public WeatherService()
+        {
+            _httpClient = new HttpClient();
+
+            _httpClient.DefaultRequestHeaders.Add("x-Api-Key", "6cwGGKJg2rUoPQzBmjE7sLhPTsQkquSkjCvXl20z");
+
+        }
+        public async Task<double> GetTemperatureAsync()
+        {
+            var response = await _httpClient.GetAsync("https://api.api-ninjas.com/v1/weather?lat=59.33&lon=18.06");
+
+            var json = await response.Content.ReadAsStringAsync();
+            var data = JsonSerializer.Deserialize<WeatherResponse>(json);
+            return data.Temperature;
+
+
+        }
+     
+
+    }
+}

6be6533318751f3fec408f6d5eef8d018ab7a89a
Elliot <233755458+yh-elliotaumalis@users.noreply.github.com>
2026-04-14 15:57:09 +0200

    Lägg till hantering av leverantörer

---
+++ WebShop/WebShop/Menus/AdminLeverantörMenu.cs
@@ -0,0 +1,45 @@
+﻿using HustlersAB.Admin.Menus;
+using WebShop.Presentation.MenuHandlers;
+
+namespace WebShop.Presentation.Menus;
+
+public class AdminLeverantörMenu : MenuBase
+{
+    private readonly AdminLeverantörHandler _handler;
+
+    public AdminLeverantörMenu(AdminLeverantörHandler handler)
+    {
+        _handler = handler;
+
+        _options = new[]
+        {
+            "Lägg till leverantör",
+            "Uppdatera leverantör",
+            "Ta bort leverantör",
+            "Tillbaka"
+        };
+    }
+
+    protected override bool ExecuteChoice(int selectedIndex)
+    {
+        switch (selectedIndex)
+        {
+            case 0:
+                _handler.HandleAddLeverantörAsync().GetAwaiter().GetResult();
+                return false;
+
+            case 1:
+                _handler.HandleUpdateLeverantörAsync().GetAwaiter().GetResult();
+                return false;
+
+            case 2:
+                _handler.HandleDeleteLeverantörAsync().GetAwaiter().GetResult();
+                return false;
+
+            case 3:
+                return true;
+        }
+
+        return false;
+    }
+}
--- WebShop/WebShop/Menus/AdminMenu.cs
+++ WebShop/WebShop/Menus/AdminMenu.cs
@@ -8,18 +8,21 @@ public class AdminMenu : MenuBase
     private readonly AdminProductHandler _productHandler;
     private readonly AdminCategoryHandler _categoryHandler;
     private readonly AdminCustomerHandler _customerHandler;
+    private readonly AdminLeverantörHandler _leverantörHandler;
 
-    public AdminMenu(AdminProductHandler productHandler, AdminCategoryHandler categoryHandler, AdminCustomerHandler customerHandler)
+    public AdminMenu(AdminProductHandler productHandler, AdminCategoryHandler categoryHandler, AdminCustomerHandler customerHandler, AdminLeverantörHandler leverantörHandler)
     {
         _productHandler = productHandler;
         _categoryHandler = categoryHandler;
         _customerHandler = customerHandler;
+        _leverantörHandler = leverantörHandler;
 
         _options = new[]
         {
             "Administrera produkter",
             "Administrera kategorier",
             "Administrera kunder",
+            "Administrera leverantörer",
             "Se statistik",
             "Tillbaka"
         };
@@ -49,11 +52,17 @@ public class AdminMenu : MenuBase
 
             case 3:
                 Console.Clear();
+                var leverantörMenu = new AdminLeverantörMenu(_leverantörHandler);
+                leverantörMenu.ShowMenu("Administrera leverantörer");
+                return false;
+
+            case 4:
+                Console.Clear();
                 Console.WriteLine("Statistik kommer senare...");
                 Console.ReadKey(true);
                 return false;
 
-            case 4:
+            case 5:
                 return true;
         }
 
